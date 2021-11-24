fafa9464e3c80b3404ca1b52548627d233fe022b
meer-habib-dev <contact.freelancermir@gmail.com>
2021-11-24 22:03:42 +0600

    doctors-portal-client

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyADb5v2RaohXdaT2UqK8QgRE4f8Xv-Q2d0",
+  authDomain: "doctors-portal-5bb1b.firebaseapp.com",
+  projectId: "doctors-portal-5bb1b",
+  storageBucket: "doctors-portal-5bb1b.appspot.com",
+  messagingSenderId: "417322263051",
+  appId: "1:417322263051:web:9f66731d21fd2074dedd0e",
+};
+export default firebaseConfig;
